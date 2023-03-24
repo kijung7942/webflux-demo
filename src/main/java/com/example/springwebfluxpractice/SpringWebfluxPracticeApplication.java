db33b59b1735bd4c3e8b8e816a895592a8261526
kijung7942@naver.com <kijung7942@naver.com>
2023-03-24 16:08:29 +0900

    ## 요약 > Domain Layer 작성
    # 새로운 기능
    1. webflux를 이용한 Repository 작성
    2. domain model 작성

---
+++ src/main/java/com/example/springwebfluxpractice/SpringWebfluxPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.example.springwebfluxpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxPracticeApplication.class, args);
+    }
+
+
+}
